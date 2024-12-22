10ccc4a553adf87d7af2c4aaea986117758b93f9
sanjaysingh0017 <85580645+sanjaysingh0017@users.noreply.github.com>
2024-12-22 14:29:06 +0530

    adding insert sql queries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student (id, marks, name) values ('15b2e389-0039-456b-8ebe-9654844c334c', 98, 'Jim');
+insert into student (id, marks, name) values ('f63b264a-96bd-4a16-ae46-1e75a1e5dcb7', 78, 'Kim');
+insert into student (id, marks, name) values ('87a0312c-1f3f-48c0-9f3b-a5d7c22e903d', 54, 'Mohan');
+insert into student (id, marks, name) values ('a96622f3-8db0-4266-b490-2ca553383238', 36, 'Karen');
